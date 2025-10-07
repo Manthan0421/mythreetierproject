61bbf85abf8f901d450d98836d4246a7dadd23c6
manthan <manthansawant0421@gmail.com>
2025-10-07 17:37:48 +0530

    js file delted

--- db_config.js
+++
@@ -1,6 +0,0 @@
-module.export = Object.freeze({
-    DB_HOST : "webappdb.cnw4gguum5kq.ap-south-1.rds.amazonaws.com"
-    DB_USER : "admin"
-    DB_PWD : "Manthan4217"
-    DB_DATABASE : "webappdb"
-})
